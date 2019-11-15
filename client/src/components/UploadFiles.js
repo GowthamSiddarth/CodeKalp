76a9966f49bafb59ecdba5b523ac08b4286c212a
Gowtham Behara <gothsiddu@gmail.com>
2019-11-15 17:32:27 +0530

    Remove default code and render basic Upload component

---
+++ client/src/components/UploadFiles.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class UploadFiles extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <p>Upload Files</p>
+        );
+    }
+}
+
+export default UploadFiles;
